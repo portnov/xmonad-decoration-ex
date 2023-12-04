9c14a0df53f73dd65ad6965b0403b5c5bdfb1105
Ilya V. Portnov <portnov@bk.ru>
2023-12-04 23:40:57 +0500

    Add general module.

---
+++ src/XMonad/Layout/DecorationEx.hs
@@ -0,0 +1,17 @@
+
+module XMonad.Layout.DecorationEx (
+    module XMonad.Layout.DecorationEx.Types,
+    module XMonad.Layout.DecorationEx.DecorationStyleEx,
+    module XMonad.Layout.DecorationEx.LayoutModifier,
+    module XMonad.Layout.DecorationEx.Widgets,
+    module XMonad.Layout.DecorationEx.TextDecoration,
+    module XMonad.Layout.DecorationEx.TabbedTextDecoration
+  ) where
+
+import XMonad.Layout.DecorationEx.Types
+import XMonad.Layout.DecorationEx.DecorationStyleEx
+import XMonad.Layout.DecorationEx.LayoutModifier
+import XMonad.Layout.DecorationEx.Widgets
+import XMonad.Layout.DecorationEx.TextDecoration
+import XMonad.Layout.DecorationEx.TabbedTextDecoration
+
